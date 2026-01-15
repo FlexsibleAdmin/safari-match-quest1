3cd2a6225a0eb4b3d49fba30f672f43538979ee4
Aurelia <aurelia-bot@aurelia.com>
2026-01-15 23:48:45 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,10 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add canvas-confetti@^1.9.3",
+    "bun add @types/canvas-confetti@^1.6.4"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
